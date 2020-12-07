a8301da96c9c029ca81eff0d2b99322b419cd3ce
maxzkn <maks.zyk@gmail.com>
2020-12-07 18:04:25 +0200

    updated

--- Python-learning/csv_reader/new_file.py
+++ Python-learning/csv_reader/new_file.py
@@ -1,9 +1,9 @@
 import csv
 
-with open("C:\Max\Code\supermarket_sales.csv", "r") as csv_file:
+with open("C:\Max\Code\PycharmProjects\Python\csv_reader", "r") as csv_file:
     csv_reader = csv.DictReader(csv_file)
 
-    with open("C:\Max\Code\supermarket_sales_new.csv", 'w', newline='') as new_csv_file:
+    with open("C:\Max\Code\PycharmProjects\Python\csv_reader", 'w', newline='') as new_csv_file:
         fieldnames = ['City', 'Unit price']
         csv_writer = csv.DictWriter(new_csv_file, fieldnames=fieldnames, extrasaction='ignore')
 
